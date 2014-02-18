8e19192d20cc94a4386383d91df92c45e47678d7
Mario Pierre-Louis Marín <b.mplm@tid.es>
2014-02-18 21:10:48 +0100

    updating style

--- js/contact_me.js
+++ js/contact_me.js
@@ -23,7 +23,7 @@ $(function() {
 	   firstName = name.split(' ').slice(0, -1).join(' ');
          }        
 	 $.ajax({
-                url: "./bin/contact_me.php",
+                url: "/bin/contact_me.php",
             	type: "POST",
             	data: {name: name, email: email, message: message},
             	cache: false,
